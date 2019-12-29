97d33fd0b6a7153f17cdfdbae1418352f5a730a7
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-12-29 18:23:36 -0500

    191229-1823 - Added an Event component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Blueberry app"/>
+    <Event/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Event from './components/Event.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Event
   }
 }
 </script>
---
+++ src/components/Event.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="event">
+    <p>I am one event</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Event',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.event {
+    background-color: #333;
+    padding: 1em;
+}
+
+</style>
