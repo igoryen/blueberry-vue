a09ef942ce9fd57816fe22abcc1cfada025d0cb9
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-12-29 18:41:35 -0500

    191229-1841 - can iterate through object

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="event">
     <p>I am one event</p>
+    <ul id="v-for-object" class="demo">
+        <li v-for="(value, name) in person" v-bind:key="(value,name)">
+            {{ name }}: {{ value }}
+        </li>
+    </ul>
   </div>
 </template>
 
@@ -9,8 +14,18 @@ export default {
   name: 'Event',
   props: {
     msg: String
-  }
+  },
+    data() {
+        return {
+            person: {
+                firstName: 'Igor',
+                lastName: 'Yen',
+                gender: 'male'
+            }
+        }
+    }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -18,6 +33,11 @@ export default {
 .event {
     background-color: #333;
     padding: 1em;
+}
+.demo {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
 }
 
 </style>
