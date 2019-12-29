62d7cd36bba277332d9b6495e4e7a1b1149c7bae
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-12-29 18:11:57 -0500

    191229-1811 - changed to dark theme

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,16 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+    background-color: #2c3e50;
+    color: #f2f2f2;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  
   margin-top: 60px;
 }
 </style>
