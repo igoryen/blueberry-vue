43003da864d4b2894b76efb50dd1945f5407c14c
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-12-31 12:43:57 -0500

    191230-0808 - can iterate through external json file -2

---
+++ src/components/EventsWrapper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="events">
+    <p>Events</p>
+    <div id="v-for-object" class="demo">
+        <div v-for="data in myJson" v-bind:key="data">
+            <Event :msg="data.title"/>            
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Event from './Event.vue'
+import lines from '../json/lines.json'
+export default {
+  name: 'EventsWrapper',
+  components: {
+      Event
+  },
+  props: {
+    msg: String
+  },
+    data() {
+        return {
+            myJson: lines
+        }
+    }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.events {
+    background-color: #333;
+    padding: 1em;
+}
+.demo {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+</style>
