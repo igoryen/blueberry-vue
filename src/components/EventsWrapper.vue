d5607b137c6195c436f4cb07806c49c6e3849cb7
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-12-31 13:40:02 -0500

    191231-1339 - the form can get() data

--- src/components/EventsWrapper.vue
+++ src/components/EventsWrapper.vue
@@ -10,6 +10,11 @@
         <input type="text" class="form-control" v-model="user.email">
     </div>
     <button @click="submit">Submit</button>
+    <hr>
+    <button @click="fetchData">Get Data</button>
+    <ul>
+        <li v-for="user in users" v-bind:key="user">{{ user.username}} - {{user.email}}</li>
+    </ul>
     <div id="v-for-object" class="demo">
         <div v-for="data in myJson" v-bind:key="data">
             <Event :msg="data.title"/>            
@@ -35,6 +40,7 @@ export default {
                 username: '',
                 email: ''
             },
+            users: [],
             myJson: lines
         }
     },
@@ -45,6 +51,19 @@ export default {
                 response => { console.log(response); },
                 error => { console.log(error); }
             );
+        },
+        fetchData() {
+            this.$http.get( 'https://blueberry-f0510.firebaseio.com/data.json' )
+            .then(
+                response => { return response.json(); }   
+            )
+            .then( data => {
+                const resultArray = [];
+                for ( let key in data ) {
+                    resultArray.push( data[key]);
+                }
+                this.users = resultArray;
+            });
         }
     }
 }
